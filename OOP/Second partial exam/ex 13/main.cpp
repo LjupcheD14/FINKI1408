e71b81689e252d253967c1ed93e51c84ca6a45de
Ljubomir Davitkov <81088064+LjupcheD14@users.noreply.github.com>
2022-05-31 03:16:15 +0200

    Create main.cpp

---
+++ OOP/Second partial exam/ex 13/main.cpp
@@ -0,0 +1,373 @@
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+
+
+class Image
+{
+protected:
+    char *ime;
+    char korisnik[50];
+    int sirina;
+    int visina;
+public:
+    Image(const char *ime="untitled", const char *korisnik="unknown", int sirina=800, int visina=800)
+    {
+        this->ime = new char[strlen(ime)+1];
+        strcpy(this->ime,ime);
+        strcpy(this->korisnik,korisnik);
+        this->sirina=sirina;
+        this->visina=visina;
+    }
+    virtual double fileSize()
+    {
+        return visina*sirina*3;
+    }
+    friend ostream &operator<<(ostream &out, Image &i)
+    {
+        out<<i.ime<<" "<<i.korisnik<<" "<<i.fileSize()<<endl;
+        return out;
+    }
+    friend ostream &operator<<(ostream &out, Image *i)
+    {
+        out<<i->ime<<" "<<i->korisnik<<" "<<i->fileSize()<<endl;
+        return out;
+    }
+    bool operator > (Image &i)
+    {
+        return fileSize() > i.fileSize();
+    }
+
+};
+
+class TransparentImage : public Image
+{
+private:
+    bool trans;
+public:
+    TransparentImage(const char *ime="untitled", const char *korisnik="unknown", int sirina=800, int visina=800, bool trans=false) : Image(ime,korisnik,sirina,visina)
+    {
+        this->trans=trans;
+    }
+    double fileSize()
+    {
+        if(trans==true)
+        {
+            return sirina*visina*4;
+        }
+        else
+        {
+            return (sirina*visina) + (sirina*visina)/8;
+        }
+    }
+};
+
+class Folder
+{
+private:
+    char ime[255];
+    char sopstvenik[50];
+    Image *niza[100];
+    int broj;
+public:
+    Folder(const char *ime="",const char *sopstvenik="unknown")
+    {
+        strcpy(this->ime,ime);
+        strcpy(this->sopstvenik,sopstvenik);
+        this->broj = 0;
+    }
+    int folderSize() const
+    {
+        int total=0;
+        for(int i=0; i<broj; i++)
+        {
+            total+=niza[i]->fileSize();
+        }
+        return total;
+    }
+    Image *getMaxFile()
+    {
+        Image *tmp = niza[0];
+        for(int i=1; i<broj; i++)
+        {
+            if((*niza[i])>*tmp)
+            {
+                tmp=niza[i];
+            }
+        }
+        return tmp;
+    }
+    Folder&operator+=(Image &i)
+    {
+        niza[broj++]=&i;
+        return *this;
+    }
+    friend ostream&operator<<(ostream &out, const Folder &f)
+    {
+        out<<f.ime<<" "<<f.sopstvenik<<endl;
+        out<<"--"<<endl;
+        for(int i=0; i<f.broj; i++)
+        {
+            out<<f.niza[i];
+        }
+        out<<"--"<<endl;
+        out<<"Folder size: "<<f.folderSize();
+        return out;
+    }
+    Image*operator[](int index)
+    {
+        if(index>broj)
+        {
+            return NULL;
+        }
+        return niza[index];
+    }
+};
+
+Folder max_folder_size(Folder *folders, int n)
+{
+    Folder tmp=folders[0];
+    for(int i=1; i<n; i++)
+    {
+        if(folders[i].folderSize()>tmp.folderSize())
+        {
+            tmp=folders[i];
+        }
+    }
+    return tmp;
+}
+
+int main()
+{
+
+    int tc; // Test Case
+
+    char name[255];
+    char user_name[51];
+    int w, h;
+    bool tl;
+
+    cin >> tc;
+
+    if (tc==1)
+    {
+        // Testing constructor(s) & operator << for class File
+
+        cin >> name;
+        cin >> user_name;
+        cin >> w;
+        cin >> h;
+
+
+        Image f1;
+
+        cout<< f1;
+
+        Image f2(name);
+        cout<< f2;
+
+        Image f3(name, user_name);
+        cout<< f3;
+
+        Image f4(name, user_name, w, h);
+        cout<< f4;
+    }
+    else if (tc==2)
+    {
+        // Testing constructor(s) & operator << for class TextFile
+        cin >> name;
+        cin >> user_name;
+        cin >> w >> h;
+        cin >> tl;
+
+        TransparentImage tf1;
+        cout<< tf1;
+
+        TransparentImage tf4(name, user_name, w, h, tl);
+        cout<< tf4;
+    }
+    else if (tc==3)
+    {
+        // Testing constructor(s) & operator << for class Folder
+        cin >> name;
+        cin >> user_name;
+
+        Folder f3(name, user_name);
+        cout<< f3;
+    }
+    else if (tc==4)
+    {
+        // Adding files to folder
+        cin >> name;
+        cin >> user_name;
+
+        Folder dir(name, user_name);
+
+        Image * f;
+        TransparentImage *tf;
+
+        int sub, fileType;
+
+        while (1)
+        {
+            cin >> sub; // Should we add subfiles to this folder
+            if (!sub) break;
+
+            cin >>fileType;
+            if (fileType == 1)  // Reading File
+            {
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                f = new Image(name,user_name, w, h);
+                dir += *f;
+            }
+            else if (fileType == 2)
+            {
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                cin >> tl;
+                tf = new TransparentImage(name,user_name, w, h, tl);
+                dir += *tf;
+            }
+        }
+        cout<<dir;
+    }
+    else if(tc==5)
+    {
+        // Testing getMaxFile for folder
+
+        cin >> name;
+        cin >> user_name;
+
+        Folder dir(name, user_name);
+
+        Image* f;
+        TransparentImage* tf;
+
+        int sub, fileType;
+
+        while (1)
+        {
+            cin >> sub; // Should we add subfiles to this folder
+            if (!sub) break;
+
+            cin >>fileType;
+            if (fileType == 1)  // Reading File
+            {
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                f = new Image(name,user_name, w, h);
+                dir += *f;
+            }
+            else if (fileType == 2)
+            {
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                cin >> tl;
+                tf = new TransparentImage(name,user_name, w, h, tl);
+                dir += *tf;
+            }
+        }
+        cout<< *(dir.getMaxFile());
+    }
+    else if(tc==6)
+    {
+        // Testing operator [] for folder
+
+        cin >> name;
+        cin >> user_name;
+
+        Folder dir(name, user_name);
+
+        Image* f;
+        TransparentImage* tf;
+
+        int sub, fileType;
+
+        while (1)
+        {
+            cin >> sub; // Should we add subfiles to this folder
+            if (!sub) break;
+
+            cin >>fileType;
+            if (fileType == 1)  // Reading File
+            {
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                f = new Image(name,user_name, w, h);
+                dir += *f;
+            }
+            else if (fileType == 2)
+            {
+                cin >> name;
+                cin >> user_name;
+                cin >> w >> h;
+                cin >> tl;
+                tf = new TransparentImage(name,user_name, w, h, tl);
+                dir += *tf;
+            }
+        }
+
+        cin >> sub; // Reading index of specific file
+        cout<< *dir[sub];
+    }
+    else if(tc==7)
+    {
+        // Testing function max_folder_size
+        int folders_num;
+
+        Folder dir_list[10];
+
+        Folder dir;
+        cin >> folders_num;
+
+        for (int i=0; i<folders_num; ++i)
+        {
+            cin >> name;
+            cin >> user_name;
+            dir = Folder(name, user_name);
+
+
+            Image* f;
+            TransparentImage *tf;
+
+            int sub, fileType;
+
+            while (1)
+            {
+                cin >> sub; // Should we add subfiles to this folder
+                if (!sub) break;
+
+                cin >>fileType;
+                if (fileType == 1)  // Reading File
+                {
+                    cin >> name;
+                    cin >> user_name;
+                    cin >> w >> h;
+                    f = new Image(name,user_name, w, h);
+                    dir += *f;
+                }
+                else if (fileType == 2)
+                {
+                    cin >> name;
+                    cin >> user_name;
+                    cin >> w >> h;
+                    cin >> tl;
+                    tf = new TransparentImage(name,user_name, w, h, tl);
+                    dir += *tf;
+                }
+            }
+            dir_list[i] = dir;
+        }
+
+        cout<<max_folder_size(dir_list, folders_num);
+    }
+    return 0;
+};
