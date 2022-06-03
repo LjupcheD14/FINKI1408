2e62e8bd7614e08ea46d23e22186e02641f46659
Ljubomir Davitkov <81088064+LjupcheD14@users.noreply.github.com>
2022-06-03 02:10:09 +0200

    Update Important.cpp

--- OOP/Second partial exam/Important.cpp
+++ OOP/Second partial exam/Important.cpp
@@ -24,7 +24,19 @@ Complex operator + (Complex const &obj) {
          res.imag = imag + obj.imag;
          return res;
     }
-///Operator +=
+///Operator +=MobileServis&operator+=(Device &d){
+        if(d.getGodina()>2019 || d.getGodina()<2000){
+            throw InvalidProductionDate();
+        }
+        Device *tmp = new Device[broj+1];
+        for(int i=0;i<broj;i++){
+            tmp[i]=niza[i];
+        }
+        tmp[broj++]=d;
+        delete[]niza;
+        niza=tmp;
+        return *this;
+    }
 FINKI_bookstore &operator+=(Customer &c)
     {
         for(int i=0; i<num; i++)
@@ -162,4 +174,97 @@ bool prebarajKoncert(Koncert ** koncerti, int n, char * naziv, bool elektronski)
         }
     }
     return false;
+}
+///Set static void
+static void setJournal(int number)
+    {
+        T=number;
+    }
+int PhDStudent::T = 3;
+///Функција void najniskaCena() со која се печати огласот што има најниска цена. Ако има повеќе огласи со иста најниска цена, да се испечатат податоците за првиот од нив.
+void najniskaCena()
+    {
+        Oglas temp = niza[0];
+        for(int i=1;i<broj;i++)
+        {
+            if(niza[i].getCena() < temp.getCena())
+            {
+                temp=niza[i];
+            }
+        }
+        cout<<temp;
+    }
+///Функција void oglasOdKategorija(const char *k) со која се печатат сите огласи од категоријата k што е проследена како влезен аргумент на методот.
+void oglasiOdKategorija(const char *k)
+    {
+        for(int i=0;i<broj;i++)
+        {
+            if(strcmp(niza[i].getKategorija(),k) == 0)
+            {
+                cout<<niza[i]<<endl;
+            }
+        }
+    }
+///Да се дефинира надворешна функција brojPretstaviNaDelo која како аргумент прима низа од покажувачи кон претстави кои се прикажале во еден месец, големина на низата и едно дело. Оваа функција пресметува на колку од дадените претстави е прикажано даденото дело.
+int brojPretstaviNaDelo (Pretstava **niza, int n, Delo d){
+    int vkupno=0;
+    for(int i=0;i<n;i++){
+        if(niza[i]->getDelo()==d){
+            vkupno++;
+        }
+    }
+    return vkupno;
+}
+///Да се имплементира функција vkupno_SMS со потпис:
+///void vkupno_SMS(SMS** poraka, int n)
+///во која се печати вкупниот број на регуларни СМС пораки и нивната вкупна цена, како и бројот на специјални СМС пораки и нивната вкупна цена во проследената низа посебно.
+void vkupno_SMS(SMS **poraka, int n){
+    double vkupnoREG=0,vkupnoSPEC=0;
+    int brojacREG=0,brojacSPEC=0;
+    for(int i=0;i<n;i++){
+        SpecialSMS *tmp = dynamic_cast<SpecialSMS*>(poraka[i]);
+        if(tmp!=0){
+            brojacSPEC++;
+            vkupnoSPEC+=tmp->SMS_cena();
+        } else {
+            brojacREG++;
+            vkupnoREG+=poraka[i]->SMS_cena();
+        }
+    }
+    cout<<"Vkupno ima "<<brojacREG<<" regularni SMS poraki i nivnata cena e: "<<vkupnoREG<<endl;
+    cout<<"Vkupno ima "<<brojacSPEC<<" specijalni SMS poraki i nivnata cena e: "<<vkupnoSPEC<<endl;
+}
+///функција postaviOpisnaOcenka(char * ime, char* opisnaOcenka) во која на студентот на курсот со даденото име му се поствува описна оценка. Ако тоа не е можно функцијата не прави ништо
+void postaviOpisnaOcenka(const char *ime,const char *opisnaOcenka)
+    {
+        for(int i=0;i<broj;i++)
+        {
+            if(strcmp(studenti[i]->getIme(),ime) == 0 && studenti[i]->getDaliUsno() == true)
+            {
+                StudentKursUsno *temp=dynamic_cast<StudentKursUsno*>(studenti[i]);
+                if(temp != 0)
+                {
+                    *temp+=opisnaOcenka;
+                    break;
+                }
+            }
+        }
+    }
+///Да се имплементира функција pecatiPoloshiPonudi што на влез прима низа од понуди за транспорт на патник, бројот на елементите од низата и една понуда T. Функцијата ја печати дестинацијата, растојанието до дестинацијата и цената за понудите од низата кои се поскапи од понудата T сортирани во растечки редослед по растојанието до дестинацијата (погледни го повикот на функцијата)
+void pecatiPoloshiPonudi (Transport **ponudi, int n, Transport &t){
+    Transport *tmp;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(ponudi[i]->cena() > ponudi[j]->cena()){
+                tmp=ponudi[i];
+                ponudi[i]=ponudi[j];
+                ponudi[j]=tmp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(ponudi[i]->cena() > t.cena()){
+            cout<<*ponudi[i];
+        }
+    }
 }
