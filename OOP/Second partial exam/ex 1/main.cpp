c6cb41b3d59e3af9b0c3f97fae6a5f923e093a5c
Ljubomir Davitkov <81088064+LjupcheD14@users.noreply.github.com>
2022-08-22 15:30:15 +0200

    Update main.cpp

--- OOP/Second partial exam/ex 1/main.cpp
+++ OOP/Second partial exam/ex 1/main.cpp
@@ -4,104 +4,84 @@
 
 using namespace std;
 
-class FINKI_Education
-{
+class FINKI_Education{
 protected:
-    char ISBN[20];
+    char isbn[20];
     char naslov[50];
     char avtor[30];
     double osnovna_cena;
 public:
-    FINKI_Education(char ISBN[20], char naslov[50], char avtor[30], double osnovna_cena)
-    {
-        strcpy(this->ISBN,ISBN);
+    FINKI_Education(char isbn[20], char naslov[50], char avtor[30], double osnovna_cena){
+        strcpy(this->isbn,isbn);
         strcpy(this->naslov,naslov);
         strcpy(this->avtor,avtor);
         this->osnovna_cena=osnovna_cena;
     }
     virtual double bookPrice()=0;
 
-    bool operator>(FINKI_Education &finki)
-    {
-        return bookPrice()>finki.bookPrice();
+    bool operator > (FINKI_Education &f){
+        return bookPrice() > f.bookPrice();
     }
 
-    friend ostream &operator<<(ostream &out, FINKI_Education &finki)
-    {
-        out<<finki.ISBN<<": "<<finki.naslov<<", "<<finki.avtor<<" "<<finki.bookPrice()<<endl;
-        return out;
+    friend ostream &operator << (ostream &out, FINKI_Education &f){
+        return out<<f.isbn<<": "<<f.naslov<<", "<<f.avtor<<" "<<f.bookPrice()<<endl;
     }
 
-    void setISBN(char ISBN[20])
-    {
-        strcpy(this->ISBN, ISBN);
+    void setISBN(char isbn[20]){
+        strcpy(this->isbn,isbn);
     }
-
 };
 
-class Online_kniga : public FINKI_Education
-{
+class Online_kniga : public FINKI_Education{
 private:
     char *url;
-    int golemina;
+    int goleminia;
 public:
-    Online_kniga(char ISBN[20], char naslov[50], char avtor[30], double osnovna_cena, char *url,int golemina) : FINKI_Education(ISBN,naslov,avtor,osnovna_cena)
-    {
-        this->golemina=golemina;
+    Online_kniga(char isbn[20], char naslov[50], char avtor[30], double osnovna_cena, char *url, int golemina) : FINKI_Education(isbn,naslov,avtor,osnovna_cena){
+        this->goleminia=golemina;
         this->url = new char[strlen(url)];
         strcpy(this->url,url);
     }
-    double bookPrice()
-    {
-        if(golemina>20)
+    double bookPrice(){
+        if(goleminia>20)
             return osnovna_cena*1.2;
         else
             return osnovna_cena;
     }
 };
-class Pecatena_kniga : public FINKI_Education
-{
+
+class Pecatena_kniga : public FINKI_Education{
 private:
-    bool zaliha;
     double masa;
+    bool zaliha;
 public:
-    Pecatena_kniga(char ISBN[20], char naslov[50], char avtor[30], double osnovna_cena, bool zaliha=false, double masa=0) : FINKI_Education(ISBN,naslov,avtor,osnovna_cena)
-    {
+    Pecatena_kniga(char isbn[20], char naslov[50], char avtor[30], double osnovna_cena, bool zaliha=false, double masa=0) : FINKI_Education(isbn,naslov,avtor,osnovna_cena){
         this->zaliha=zaliha;
         this->masa=masa;
     }
     double bookPrice()
     {
         if(masa>0.7)
-        {
-            return osnovna_cena * 1.15;
-        }
+            return osnovna_cena*1.15;
         else
-        {
             return osnovna_cena;
-        }
     }
 };
 
-void mostExpensiveBook(FINKI_Education **knigi, int dolzina)
-{
-    FINKI_Education *najskapa = knigi[0];
-    int broj_pecateni=0, broj_online=0;
-    for(int i=0; i<dolzina; i++)
-    {
-        Online_kniga *o=dynamic_cast<Online_kniga*>(knigi[i]);
-        if(o!=0)
-        {
+void mostExpensiveBook (FINKI_Education** books, int n){
+    FINKI_Education *najskapa = books[0];
+    int broj_pecateni=0,broj_online=0;
+    for(int i=0;i<n;i++){
+        Online_kniga *o = dynamic_cast <Online_kniga*>(books[i]);
+        if(o!=0){
             broj_online++;
-            if(o->bookPrice()>najskapa->bookPrice()) najskapa=o;
-        }
-        else
-        {
-            Pecatena_kniga *p=dynamic_cast<Pecatena_kniga*>(knigi[i]);
-            if(p!=0)
-            {
+            if(o->bookPrice() > najskapa->bookPrice())
+                najskapa=o;
+        } else {
+            Pecatena_kniga *p = dynamic_cast <Pecatena_kniga*>(books[i]);
+            if(p!=0){
                 broj_pecateni++;
-                if(p->bookPrice()>najskapa->bookPrice()) najskapa=p;
+                najskapa=p;
             }
         }
     }
@@ -109,8 +89,9 @@ void mostExpensiveBook(FINKI_Education **knigi, int dolzina)
     cout<<"Total number of online books: "<<broj_online<<endl;
     cout<<"Total number of print books: "<<broj_pecateni<<endl;
     cout<<"The most expensive book is: "<<endl;
-    cout<<*mostExpensiveBook<<endl;
+    cout<<*najskapa<<endl;
 }
+
 
 
 int main()
