90fcc90411b8c180d279d4b134f96567b01c6b23
Ljubomir Davitkov <81088064+LjupcheD14@users.noreply.github.com>
2023-08-23 20:26:09 +0200

    Create funkcijarRanom.js

---
+++ IPNKS/codes/Exam/funkcijarRanom.js
@@ -0,0 +1,15 @@
+        function generateRandomString(length) {
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let result = '';
+
+            for (let i = 0; i < length; i++) {
+                const randomIndex = Math.floor(Math.random() * characters.length);
+                result += characters.charAt(randomIndex);
+            }
+
+            return result;
+        }
+
+        // Example usage: Generate a random string of length 10
+        const randomString = generateRandomString(10);
+        console.log(randomString); // Output will be a random string of length 10
