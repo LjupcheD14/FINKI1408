64ace07cfe82c66bada7302897878c448cc4e050
Ljubomir Davitkov <81088064+LjupcheD14@users.noreply.github.com>
2023-08-24 19:45:14 +0200

    Update funkcijarRanom.js

--- IPNKS/codes/Exam/funkcijarRanom.js
+++ IPNKS/codes/Exam/funkcijarRanom.js
@@ -30,3 +30,12 @@
     }
 
         const broj1 = getRandomNumber(1, 100);
+
+// Method map
+const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array1.map((x) => x * 2);
+
+console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
