e07d9c5c16b63f3c69c60912c195f29bd7bb6d1b
Ljubomir Davitkov <81088064+LjupcheD14@users.noreply.github.com>
2023-08-24 15:01:03 +0200

    Update funkcijarRanom.js

--- IPNKS/codes/Exam/funkcijarRanom.js
+++ IPNKS/codes/Exam/funkcijarRanom.js
@@ -13,3 +13,14 @@
         // Example usage: Generate a random string of length 10
         const randomString = generateRandomString(10);
         console.log(randomString); // Output will be a random string of length 10
+
+
+
+RANDOM NUMBER
+            function getRandomNumber(min, max) {
+                let step1 = max - min + 1;
+                let step2 = Math.random() * step1;
+                let result = Math.floor(step2) + min;
+
+                return result;
+            }
